afc95cf6c5f05e46fdb85b1dcf2baee47912fcea
Matheus M <m.mdamata@hotmail.com>
2026-03-02 15:48:37 -0300

    Update Atividade Sorteio

--- 41_SPRINTS/2601TG/_FILES/Atividade Sorteio/sorteio.js
+++ 41_SPRINTS/2601TG/_FILES/Atividade Sorteio/sorteio.js
@@ -1,5 +1,6 @@
 let secretNumber = Math.floor(Math.random() * 20) + 1;
 let attempts = 0;
+let isGameRunning = true;
 const maxAttempts = 5;
 
 document.getElementById("guessButton").addEventListener("click", function(){
@@ -10,19 +11,36 @@ document.getElementById("guessButton").addEventListener("click", function(){
         alert("Número inválido! Digite um número de 1 a 20");
         return;
     }
-    attempts++;
-    if(guess !== secretNumber){
-        message.textContent = `Esse não é o número sorteado, você ainda tem ${maxAttempts - attempts} tentativas`;
-        message.style.color = "orange";
+    if(isGameRunning){
+        attempts++;
+
+        if(guess !== secretNumber){
+            message.textContent = `Esse não é o número sorteado, você ainda tem ${maxAttempts - attempts} tentativas`;
+            message.style.color = "orange";
+            message.style.textShadow = "1px 1px 2px orange";
+        }
+        if(guess === secretNumber){
+            isGameRunning = false;
+            message.textContent = `Parabéns! Você acertou em ${attempts} tentativas.`;
+            message.style.color = "greenyellow";
+            message.style.textShadow = "1px 1px 2px green";
+            return;
+        }
+        if(attempts >= maxAttempts){
+            isGameRunning = false;
+            message.textContent = `Fim de jogo! O numero correto era ${secretNumber}`;
+            message.style.color = "red";
+            message.style.textShadow = "1px 1px 2px red";
+            return;
+        }
     }
-    if(guess === secretNumber){
-        message.textContent = `Parabéns! Você acertou em ${attempts} tentativas.`;
-        message.style.color = "green";
-        return;
-    }
-    if(attempts >= maxAttempts){
-        message.textContent = `Fim de jogo! O numero correto era ${secretNumber}`;
-        message.style.color = "red";
-        return;
+    if(!isGameRunning){
+        document.getElementById("restartButton").addEventListener("click", function(){
+            let newSecretNumber = Math.floor(Math.random()*20) + 1;
+            secretNumber = newSecretNumber;
+            attempts = 0;
+            message.textContent = "";
+            isGameRunning = true;
+        });
     }
 });
